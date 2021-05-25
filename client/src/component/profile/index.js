38a8430ad75174faddd956bcdb6bb0f77c85c760
Afoghandi <ralphaeloshun@gmail.com>
2021-05-26 00:31:51 +0100

    profile education and profile experience added

--- client/src/component/profile/index.js
+++ client/src/component/profile/index.js
@@ -5,6 +5,8 @@ import { getProfileById } from '../../actions/profile';
 import { Link } from 'react-router-dom';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 
 const Profile = ({ match }) => {
 	const getProfile = useSelector((state) => state.profile);
@@ -31,9 +33,39 @@ const Profile = ({ match }) => {
 								Edit Profile
 							</Link>
 						)}
-					<div class='profile-grid my-1'>
+					<div className='profile-grid my-1'>
 						<ProfileTop profile={profile} />
 						<ProfileAbout profile={profile} />
+						<div className='profile-exp bg-white p-2'>
+							<h2 className='test-primary'>Experience</h2>
+							{profile.experience.length > 0 ? (
+								<Fragment>
+									{profile.experience.map((experience) => (
+										<ProfileExperience
+											key={experience._id}
+											experience={experience}
+										/>
+									))}
+								</Fragment>
+							) : (
+								<h4>No experience credentials </h4>
+							)}
+						</div>
+						<div className='profile-edu bg-white p-2'>
+							<h2 className='test-primary'>Education</h2>
+							{profile.education.length > 0 ? (
+								<Fragment>
+									{profile.education.map((education) => (
+										<ProfileEducation
+											key={education._id}
+											education={education}
+										/>
+									))}
+								</Fragment>
+							) : (
+								<h4>No education credentials </h4>
+							)}
+						</div>
 					</div>
 				</Fragment>
 			)}
--- client/src/component/profiles/ProfileItem.js
+++ client/src/component/profiles/ProfileItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './styles/profile.css';
+import './styles/profiles.css';
 const ProfileItem = ({
 	profile: {
 		user: { name, _id, avatar },
